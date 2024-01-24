441d81715c5589096857b139a7f846e761ae1ecb
henrique.amorim <henrique.amorim@mportal.com.br>
2024-01-24 15:23:55 -0300

    ADD - adicionado aula 13 Content Negotiation

---
+++ 13_ContentNegotiation/src/main/java/br/com/erudio/mapper/custom/PersonMapper.java
@@ -0,0 +1,33 @@
+package br.com.erudio.mapper.custom;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class PersonMapper {
+
+    public PersonVO convertEntityToVo(Person person){
+        PersonVO vo = new PersonVO();
+        vo.setId(person.getId());
+        vo.setAddress(person.getAddress());
+        vo.setFirstName(person.getFirstName());
+        vo.setLastName(person.getLastName());
+        vo.setGender(person.getGender());
+        return vo;
+    }
+
+    public Person convertVoToEntity(PersonVO person){
+        Person entity = new Person();
+        entity.setId(person.getId());
+        entity.setAddress(person.getAddress());
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setGender(person.getGender());
+//        entity.setBirthDay(new Date());
+
+        return entity;
+    }
+}
