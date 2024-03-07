52ceccc0019828ef7920ee8d046856629825f767
henrique.amorim <henrique.amorim@mportal.com.br>
2024-03-07 15:52:45 -0300

    feat - adiciona HATEAOS e Swagger

---
+++ 15_Swagger/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,48 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.services.PersonServices;
+import br.com.erudio.util.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+    public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
+        return service.findById(id);
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+    public List<PersonVO> findByAll() {
+        return service.findByAll();
+    }
+
+    @PostMapping(produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+    public PersonVO create(@RequestBody PersonVO person) throws Exception {
+        return service.create(person);
+    }
+
+    @PutMapping(produces = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
+    public PersonVO update(@RequestBody PersonVO person) throws Exception {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
