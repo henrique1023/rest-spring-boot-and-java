c4c8846630a5aa061cb4131e3432a65a74456764
Henrique1023 <henrique.amorim@fatec.sp.gov.br>
2024-03-23 15:34:05 -0300

    feat - implementa UserRepository e UserServices

---
+++ 17_implementando_JWT/src/main/java/br/com/erudio/services/UserServices.java
@@ -0,0 +1,50 @@
+package br.com.erudio.services;
+
+import br.com.erudio.controllers.PersonController;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.exceptions.RequiredObjectisNullException;
+import br.com.erudio.exceptions.ResponseEntityExceptionHandler;
+import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.mapper.custom.PersonMapper;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+import br.com.erudio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class UserServices implements UserDetailsService {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(UserServices.class.getName());
+
+    @Autowired
+    UserRepository repository;
+
+    public UserServices(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Autowired
+    PersonMapper mapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Finding one User by name!");
+        var user = repository.findByUsername(username);
+        if (user != null){
+            return user;
+        }else {
+            throw new UsernameNotFoundException("" + username + " not found");
+        }
+    }
+}
