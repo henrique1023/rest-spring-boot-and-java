a3bc5a296e79a7ff271548316ceddaf4c4ada2e6
Henrique1023 <henrique.amorim@fatec.sp.gov.br>
2024-03-23 14:53:46 -0300

    fix - adicionando JWT corretamente

--- 17_Implementando_JWT/src/main/resources/db/migration/V1__Create_Table_person.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists `person`
-(
-    `id` bigint(20) not null auto_increment,
-    `first_name` varchar(80)  not null,
-    `last_name`  varchar(80)  not null,
-    `address`    varchar(100) not null,
-    `gender`     varchar(7)   not null,
-    primary key (`id`)
-);
--- 17_Implementando_JWT/src/main/resources/db/migration/V2__Population_Table_person.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `person` (id, address, first_name, gender, last_name) values
-       (1,'Sao paulo', 'Joao', 'Male', 'Silva');
-
