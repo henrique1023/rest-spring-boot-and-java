54fe7da71f40909a9542459a691ca6e68151f28f
Henrique <henrique.amorim@oneconsultoria.net>
2024-05-31 13:57:45 -0300

    feat - adiciona arquivos de configuração

---
+++ 22_UploadDownloadArquivos/src/main/java/br/com/erudio/config/FileStorageConfig.java
@@ -0,0 +1,21 @@
+package br.com.erudio.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+public class FileStorageConfig {
+
+    @Value("${file.upload-dir:default}")
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
